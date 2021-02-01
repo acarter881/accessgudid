7caf2fb9df0324d9848be230452ba4d54209c294
Alex Carter <60497471+acarter881@users.noreply.github.com>
2021-01-31 19:33:11 -0600

    Create accessgudid.py

---
+++ accessgudid.py
@@ -0,0 +1,50 @@
+#! python3
+import json
+import requests
+import pandas as pd
+
+# Reads each line of a text file as a medical device identifier
+med_devices = open('list.txt', 'r')
+lines = med_devices.readlines()
+
+# Start reading from the first line of the text file
+devicePos = 0
+
+# Create an empty list that stores all of the medical device data 
+records = []
+
+for i in range(len(lines)):
+    # Base URL for the medical device lookup in JSON format
+    URL = 'https://accessgudid.nlm.nih.gov/api/v2/devices/lookup.json?di='
+
+    # Append the device identifier to the base URL to form the completed URL
+    URL += lines[devicePos].strip()
+
+    # Increment devicePos, so the script goes to the next medical device identifier on its next iteration
+    devicePos += 1
+    
+    # Create the response object and check if the request is successful
+    res = requests.get(URL)
+    res.raise_for_status()
+
+    # Deserialize the response object's text property (a string) to a variable named deviceData
+    deviceData = json.loads(res.text)
+
+    # Relevant data to gather
+    companyName = deviceData['gudid']['device']['companyName']
+    deviceID = deviceData['gudid']['device']['identifiers']['identifier'][0]['deviceId']
+    rxUse = deviceData['gudid']['device']['rx']
+    singleUse = deviceData['gudid']['device']['singleUse']
+    gmdnPTDefinition = deviceData['gudid']['device']['gmdnTerms']['gmdn'][0]['gmdnPTDefinition']
+
+    # Append relevant data to list
+    records.append((companyName, deviceID, rxUse, singleUse, gmdnPTDefinition))
+
+# Create pandas data frame    
+df = pd.DataFrame(records, columns=['Company Name', 'Device ID', 'Prescription Use', 'Single Use', 'Definition'])
+
+# Write relevant data to an Excel file
+df.to_excel('testing.xlsx', index=False)
+
+# Close the text file
+med_devices.close()
